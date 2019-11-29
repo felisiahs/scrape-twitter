248d84060408ddd145c9343c82f79877b6876573
felisiahs <felisiasilalahi97@gmail.com>
2019-11-29 10:32:14 +0700

    new file:   allNew.csv 	new file:   allNewProcessed.csv 	new file:   import nltk.classify.py 	new file:   import.py 	new file:   nltk.classify.py 	new file:   output1.csv 	new file:   preprocess.py 	new file:   twitter-feli.csv 	new file:   twitter.csv 	new file:   twitter1.csv 	new file:   twitterX.csv 	new file:   twitter_scrape.py

---
+++ import.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import CountVectorizer
+import nltk 
+import string
+import re
+pd.set_option('display.max_colwidth', 100)
---
+++ preprocess.py
@@ -0,0 +1,51 @@
+import re
+import codecs
+import csv
+
+#start process_tweet
+def processTweet(tweet):
+    tweet = tweet.lower()
+    tweet = " " + tweet
+    tweet = re.sub(r'[^\x00-\x7F]+','', tweet)
+    #tweet = tweet.replace(" rt "," ")
+    tweet = re.sub(' rt ','', tweet)
+    tweet = re.sub('(\.)+','.', tweet)
+    #tweet = re.sub('((www\.[^\s]+)|(https://[^\s]+) | (http://[^\s]+))','URL',tweet)
+    tweet = re.sub('((www\.[^\s]+))','',tweet)
+    tweet = re.sub('((http://[^\s]+))','',tweet)
+    tweet = re.sub('((https://[^\s]+))','',tweet)
+    tweet = re.sub('@[^\s]+','',tweet)
+    tweet = re.sub('[\s]+', ' ', tweet)
+    tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+    tweet = re.sub('_','',tweet)
+    tweet = re.sub('\$','',tweet)
+    tweet = re.sub('%','',tweet)
+    tweet = re.sub('^','',tweet)
+    tweet = re.sub('&','',tweet)
+    tweet = re.sub('\*','',tweet)
+    tweet = re.sub('\(','',tweet)
+    tweet = re.sub('\)','',tweet)
+    tweet = re.sub('-','',tweet)
+    tweet = re.sub('\+','',tweet)
+    tweet = re.sub('=','',tweet)
+    tweet = re.sub('"','',tweet)
+    tweet = re.sub('~','',tweet)
+    tweet = re.sub('`','',tweet)
+    tweet = re.sub('!','',tweet)
+    tweet = re.sub(':','',tweet)
+    tweet = re.sub('^-?[0-9]+$','', tweet)
+    tweet = tweet.strip('\'"')
+    return tweet
+#end
+
+p = codecs.open('allNew.csv', encoding = 'latin-1')
+read = csv.reader(p)
+outputFile = open('output1.csv', 'w',newline='')
+outputWriter = csv.writer(outputFile)
+for row in read :
+    processedTweet = processTweet(row[8])
+    outputWriter.writerow([processedTweet])
+
+outputFile.close();
+p.close();
+print("Done !")
